8054f899ded6367cf5b68f276996a630c95e8b08
Sazib204061 <mdsaziburrahman440@gmail.com>
2025-07-05 00:00:57 +0600

    Implement VillaController and Create new Villa successfully. Include client and server side validation.

---
+++ VillaBD.Web/Controllers/VillaController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using VillaBD.Domain.Entities;
+using VillaBD.Infrastructure.Data;
+
+namespace VillaBD.Web.Controllers
+{
+    public class VillaController : Controller
+    {
+
+        private readonly ApplicationDbContext _db;
+        public VillaController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            var villas = _db.Villas.ToList();
+            return View(villas);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Villa newVilla)
+        {
+            if(newVilla.Name==newVilla.Description)
+            {
+                ModelState.AddModelError("name", "The Descriptioin cannot exactly match the Name.");
+                ModelState.AddModelError("Description", "The Descriptioin cannot exactly match the Name.");
+            }
+            if (ModelState.IsValid)
+            {
+                _db.Villas.Add(newVilla);
+                _db.SaveChanges();
+                return RedirectToAction("Index", "Villa");
+            }
+            else
+            {
+                return View();
+            }
+        }
+    }
+}
